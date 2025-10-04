a9687e226e81d04d942ae05ce3b525a5341fb7eb
Henri Schulz <henrischulz@raovq-27.ten.whka.de>
2025-10-04 10:53:48 +0200

    change canvas size

--- src/constants/canvas.js
+++ src/constants/canvas.js
@@ -1,6 +1,6 @@
 export const CANVAS_HEIGHT = 1000;
 export const CANVAS_WIDTH = 1200;
-export const CANVAS_VIEWPORT_WIDTH = 800;
+export const CANVAS_VIEWPORT_WIDTH = 500;
 export const CANVAS_VIEWPORT_HEIGHT = 500;
 export const DEFAULT_SCALE = 70; // 1 Meter = 70 Pixel
 export const SNAP_THRESHOLD_PX = 6;
